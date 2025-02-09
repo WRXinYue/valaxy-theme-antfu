2107977c6557cf555f2dba1e8268fa95c133a66d
WRXinYue <WRXinYue@users.noreply.github.com>
2025-02-09 18:03:40 +0000

    deploy: 18ef4291b03134a3712b6cfbc6f108e79c14ea86

---
+++ assets/_...path_.B63d4k38.js
@@ -0,0 +1,1 @@
+import{u as a,d as s,f as t}from"./chunks/vue-router.Cja9IFaU.js";import{x as n,a4 as r,L as c,H as p}from"./framework.DI4udANs.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
---
+++ assets/post.C9NIQyp6.js
@@ -0,0 +1,1 @@
+import{E as o,I as t,G as c,H as n}from"./framework.DI4udANs.js";const r={};function s(_,a){const e=t("RouterView");return n(),c(e)}const f=o(r,[["render",s]]);export{f as default};
